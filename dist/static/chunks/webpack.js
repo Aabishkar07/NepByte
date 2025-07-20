b3d0c310f9416ca4f41d1bd0eb6fe1161686a632
Aabishkar Shrestha <Aabishkar@adsaro.com>
2025-07-20 16:50:27 +0545

    Updated popup section in admin

--- dist/static/chunks/webpack.js
+++ dist/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d83fc6fbfa5082f7"; }
+/******/ 		__webpack_require__.h = function() { return "7bbb7ab0a4e1fa15"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
